65da65d13c7426f4e5c52ded9a2c61a64268230c
tool-japan <info@amproud.jp>
2025-04-06 13:48:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -117,3 +117,7 @@ if __name__ == "__main__":
         db.create_all()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
+with app.app_context():
+    db.drop_all()
+    db.create_all()
